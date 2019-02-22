51068427ca4afa5dc338719911753fc0b0183c1a
DESKTOP-7JNM90H\kelvi <kelvinang94@gmail.com>
2019-02-22 13:24:36 +0800

    sendingData in class format with command

--- src/GroupController.java
+++ src/GroupController.java
@@ -4,17 +4,41 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.imageio.ImageIO;
+import javax.swing.JTextArea;
 
 import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
 public class GroupController {
+	
+	
+	JTextArea messageTextArea;
+	
+	public GroupController(JTextArea messageTextArea) {
+		this.messageTextArea = messageTextArea;
+	}
+	
+	InetAddress multicastGroup = null;
+	MulticastSocket multicastSocket = null;
+	private String userID = "anonymous";
+	
+	List<User> userList = new ArrayList<>();
+	String onlineUser[] = { "" };
+	
+	List<Group> groupList = new ArrayList<>();
+	Group adminRoom = new Group("230.1.1.1", "AdminRoom");
+	
+	
+	
+	
 	public String IPincrease(String MaxIP) {
 		System.out.println(MaxIP);
 		String data[];
@@ -93,64 +117,125 @@ public class GroupController {
     }
 	
 	
-//	public void connection() {
-//		try {
-//			groupList.add(adminRoom);
-//			multicastGroup = InetAddress.getByName(groupList.get(0).IPAddress);
-//			System.out.print(multicastGroup);
-//			multicastSocket = new MulticastSocket(6789);
-//			multicastSocket.joinGroup(multicastGroup);
-//
-//			String message = userID + " joined";
-//
-//			byte[] buf = message.getBytes();
-//
-//			DatagramPacket dgpConnected = new DatagramPacket(buf, buf.length, multicastGroup, 6789);
-//			multicastSocket.send(dgpConnected);
-//			new Thread(new Runnable() {
-//
-//				@Override
-//				public void run() {
-//					byte buf1[] = new byte[1000];
-//					DatagramPacket dgpRecevied = new DatagramPacket(buf1, buf1.length);
-//					while (true) {
-//						try {
-//							multicastSocket.receive(dgpRecevied);
-//							byte[] receiveData = dgpRecevied.getData();
-//
-//							int length = dgpRecevied.getLength();
-//
-//							String msg = new String(receiveData, 0, length);
-//							
-//							
-//							if (msg.substring(0, 3).matches("^[a-zA-Z0-9_]*$")) {
-//								messageTextArea.append(msg + "\n");
-//							}
-//							
-//							if (!(msg.substring(0, 3).matches("^[a-zA-Z0-9_]*$"))) {
-//								ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(receiveData));
-//								try {
-//									groupList = (List<Group>) ois.readObject();
-//
-//								} catch (ClassNotFoundException e) {
-//									// TODO Auto-generated catch block
-//									e.printStackTrace();
-//								} finally {
-//									ois.close();
-//								}
-//							}
-//
-//							
-//
-//						} catch (IOException ex) {
-//							ex.printStackTrace();
-//						}
-//					}
-//				}
-//			}).start();
-//		} catch (IOException ex) {
-//
-//		}
-//	}
+	public void connection() {
+		DataSend sendingData = new DataSend();
+		List<String> stringData = new ArrayList<>();
+		
+		
+		try {
+			groupList.add(adminRoom);
+			multicastGroup = InetAddress.getByName(groupList.get(0).IPAddress);
+			System.out.print(multicastGroup);
+			multicastSocket = new MulticastSocket(6789);
+			multicastSocket.joinGroup(multicastGroup);
+
+			
+			sendingData.setCommand("Send");
+			
+			String message = userID + " joined";
+			
+			stringData.add(message);
+			sendingData.setStringData(stringData);
+
+			byte[] buf = toByte(sendingData);
+
+			DatagramPacket dgpConnected = new DatagramPacket(buf, buf.length, multicastGroup, 6789);
+			multicastSocket.send(dgpConnected);
+			newThread();
+		} catch (IOException ex) {
+
+		}
+	}
+	
+	
+	public void newThread() {
+		new Thread(new Runnable() {
+
+			DataSend objectDataRecieved;
+			@Override
+			public void run() {
+				byte buf1[] = new byte[10000];
+				DatagramPacket dgpRecevied = new DatagramPacket(buf1, buf1.length);
+				while (true) {
+					try {
+						multicastSocket.receive(dgpRecevied);
+						byte[] receiveData = dgpRecevied.getData();
+
+						int length = dgpRecevied.getLength();
+
+						String msg = new String(receiveData, 0, length);
+						
+						ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(receiveData));
+						try {
+							objectDataRecieved = (DataSend) ois.readObject();
+							
+
+						} catch (ClassNotFoundException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} finally {
+							ois.close();
+						}
+						
+						if (objectDataRecieved.command.equals("Send")) {
+							for(String data : objectDataRecieved.stringData) {
+								messageTextArea.append(data + "\n");
+							}
+							
+						}
+						
+						
+
+						
+
+					} catch (IOException ex) {
+						ex.printStackTrace();
+					}
+				}
+			}
+		}).start();
+	}
+	
+	
+	public byte[] toByte(DataSend datasend) {
+		byte[] buf = new byte[10000];
+		try {
+
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			ObjectOutputStream oos = new ObjectOutputStream(bos);
+			oos.writeObject(datasend);
+			buf = bos.toByteArray();
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		return buf;
+	}
+	
+	
+	public void sendMessage(String message) {
+		try {
+			DataSend sendingData = new DataSend();
+			List<String> stringData = new ArrayList<>();
+			
+			
+			String msg = message;
+			msg = userID + " : " + msg;
+			stringData.add(msg);
+			sendingData.setStringData(stringData);
+			sendingData.setCommand("Send");
+			byte[] buf = toByte(sendingData);
+			
+			DatagramPacket dgpSend = new DatagramPacket(buf, buf.length, multicastGroup, 6789);
+			multicastSocket.send(dgpSend);
+			System.out.println(multicastGroup);
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	
+	
 
 }
